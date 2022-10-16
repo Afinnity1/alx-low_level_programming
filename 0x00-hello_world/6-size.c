62dfa31b0c33fa516c2b54ba61c3c765ffa1a394
Afinnity1 <omotolafinni089@gmail.com>
2022-10-15 17:30:11 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,12 @@ int main(void)
 	int i;
 	float f;
 	char c;
-	long l;
+	long int l;
+	long long int k;
 	printf("Size of int: %lu.\n", (unsigned long) sizeof(i));
 	printf("Size of float: %lu.\n", (unsigned long) sizeof(f));
 	printf("Size of char: %lu.\n", (unsigned long) sizeof(c));
-	printf("Size of long: %lu.\n", (unsigned long) sizeof(l));
+	printf("Size of long int: %lu.\n", (unsigned long) sizeof(l));
+	printf("Size of long long int: %lu.\n", (unsigned long) sizeof(k));
 	return (0);
 }
