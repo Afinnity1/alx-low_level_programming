a69035a807642b59a6437c474f723d3a07c769c0
Afinnity1 <omotolafinni089@gmail.com>
2022-10-19 15:55:15 -0700

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+
+	char i = 'a';
+	int j = 0;
+	while (j < 10)
+{
+	i = 'a';
+	while (i <= 'z')
+{
+	_putchar(i)
+	i++;
+}
+	_putchar('\n');
+	i++;
+}
+
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lower character
+ * @c: the character to check
+ * Return: 1 if c if lowercase, 0 otherwise
+ */
+int _islower(intc)
+{
+	return (c >= 'a' && c <= 'z');
+}
